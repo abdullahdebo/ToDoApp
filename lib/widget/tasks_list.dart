e81e5cbc10e0f1ed2008147a0c435725e10557d4
ABDULLAH DEBO <abd.valancia2@gmail.com>
2024-06-12 00:53:09 +0300

    Using Consumer Widget From Provider Package

--- lib/widget/tasks_list.dart
+++ lib/widget/tasks_list.dart
@@ -8,34 +8,22 @@ import 'package:todaydo_app/models/task_data.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: Provider.of<TasksData>(context).tasks.length,
-      itemBuilder: (context, index) {
-        return TaskTile(
-            isCheked: Provider.of<TasksData>(context).tasks[index].isDone,
-            taskTitle: Provider.of<TasksData>(context).tasks[index].name,
-            checkboxChanged: (newValue) {
-              // setState(() {
-              //   widget.tasks[index].doneChange();
-              // });
-            });
+    return Consumer<TasksData>(
+      builder: (context, taskdata, child) {
+        return ListView.builder(
+          itemCount: taskdata.tasks.length,
+          itemBuilder: (context, index) {
+            return TaskTile(
+                isCheked: taskdata.tasks[index].isDone,
+                taskTitle: taskdata.tasks[index].name,
+                checkboxChanged: (newValue) {
+                  // setState(() {
+                  //   widget.tasks[index].doneChange();
+                  // });
+                });
+          },
+        );
       },
     );
-    // return ListView(
-    //   children: [
-    //     TaskTile(
-    //       taskTitle: tasks[0].name,
-    //       isCheked: tasks[0].isDone,
-    //     ),
-    //     TaskTile(
-    //       taskTitle: tasks[1].name,
-    //       isCheked: tasks[1].isDone,
-    //     ),
-    //     TaskTile(
-    //       taskTitle: tasks[2].name,
-    //       isCheked: tasks[2].isDone,
-    //     ),
-    //   ],
-    // );
   }
 }
